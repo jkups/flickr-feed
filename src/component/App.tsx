83c65a6e36e8659434aa1ad61e04714c78fc3447
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-07-16 09:44:07 +1000

    Initial commit

---
+++ src/component/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <h1 className="font-bold text-2xl">Flickr Public Feed</h1>
+        <p>
+          Search and display photo feeds, showing a thumbnail, its creation
+          date, author and any available tags.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,9 @@
-import ReactDOM from 'react-dom/client'
-import "./styles/tailwind.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./component/App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+);
