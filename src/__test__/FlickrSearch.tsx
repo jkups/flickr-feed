458d060a05b8b95fb7ec4ebc22997eed379ccd43
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-07-17 09:19:19 +1000

    Initial component test

--- src/__test__/FlickrSearch.tsx
+++ src/__test__/FlickrSearch.tsx
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react";
-import App from "../component/App";
+import { FlickrSearch } from "../component/FlickrSearch";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<FlickrSearch term={"car"} />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
