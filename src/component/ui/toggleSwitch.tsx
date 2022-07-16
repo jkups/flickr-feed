09f1aa5c5697e1e9340f557036cfd7a45c2d6f2f
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-07-16 21:01:56 +1000

    Add toggle UI component

---
+++ src/component/ui/toggleSwitch.tsx
@@ -0,0 +1,22 @@
+import { clsx } from "clsx";
+
+interface ToggleSwitchProps {
+  right: boolean;
+  onClick: () => void;
+}
+
+export const ToggleSwitch = ({ right, onClick }: ToggleSwitchProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className="w-10 h-5 rounded-full cursor-pointer bg-sky-500 relative"
+    >
+      <div
+        className={clsx(
+          "rounded-full h-5 w-5 bg-white border-2 border-sky-500 absolute",
+          right && "right-0"
+        )}
+      ></div>
+    </div>
+  );
+};
