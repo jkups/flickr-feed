62bcbca01449e3fc0f2b3995bb90bfb74863d412
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-07-16 20:59:31 +1000

    Update search result component

--- src/component/SearchResultGridContainer.tsx
+++ src/component/SearchResultGridContainer.tsx
@@ -1,20 +1,23 @@
 import { flickrFeedItemInterface } from "./utility/extractFlickrFeedData";
-import { SearchResultItem } from "./SearchResultItem";
+import { SearchResultGridItem } from "./SearchResultGridItem";
 
 interface SearchResultContainerProps {
   data: flickrFeedItemInterface[];
 }
 
-export const SearchResultContainer = ({ data }: SearchResultContainerProps) => {
+export const SearchResultGridContainer = ({
+  data,
+}: SearchResultContainerProps) => {
   return (
-    <div id="container" className="max-w-screen-xl mx-auto p-x4 py-10">
-      <div className="grid grid-cols-4 gap-6">
+    <div id="container" className="max-w-screen-xl mx-auto py-6">
+      <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-4">
         {data.map((item, idx: number) => {
           return (
-            <SearchResultItem
+            <SearchResultGridItem
               key={idx}
               author={item.author}
               dateTaken={item.date_taken}
+              tags={item.tags}
               thumbnail={item.media as unknown as flickrFeedItemInterface}
             />
           );
