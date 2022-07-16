7cc88497f0d81e3837370bfdc8a35524e55cad7d
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-07-17 09:18:34 +1000

    Update components

--- src/component/SearchResultListContainer.tsx
+++ src/component/SearchResultListContainer.tsx
@@ -1,30 +1,37 @@
 import { flickrFeedItemInterface } from "./utility/extractFlickrFeedData";
 import { SearchResultListItem } from "./SearchResultListItem";
 import { Tags } from "./Tags";
+import { divide } from "lodash";
 
 interface SearchResultContainerProps {
   data: flickrFeedItemInterface[];
+  isLoading: boolean;
 }
 
 export const SearchResultListContainer = ({
   data,
+  isLoading,
 }: SearchResultContainerProps) => {
   return (
     <div id="container" className="max-w-screen-xl mx-auto py-6">
-      <div className="grid gap-6">
-        {data.map((item, idx: number) => {
-          return (
-            <SearchResultListItem
-              key={idx}
-              author={item.author}
-              dateTaken={item.date_taken}
-              thumbnail={item.media as unknown as flickrFeedItemInterface}
-            >
-              <Tags tags={item.tags} />
-            </SearchResultListItem>
-          );
-        })}
-      </div>
+      {isLoading ? (
+        <div>Loading results...</div>
+      ) : (
+        <div className="grid gap-6">
+          {data.map((item, idx: number) => {
+            return (
+              <SearchResultListItem
+                key={idx}
+                author={item.author}
+                dateTaken={item.date_taken}
+                thumbnail={item.media as unknown as flickrFeedItemInterface}
+              >
+                <Tags tags={item.tags} />
+              </SearchResultListItem>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/component/ui/toggleSwitch.tsx
+++ src/component/ui/toggleSwitch.tsx
@@ -9,11 +9,11 @@ export const ToggleSwitch = ({ right, onClick }: ToggleSwitchProps) => {
   return (
     <div
       onClick={onClick}
-      className="w-10 h-5 rounded-full cursor-pointer bg-sky-500 relative"
+      className="w-10 h-5 rounded-full cursor-pointer bg-sky-600 relative"
     >
       <div
         className={clsx(
-          "rounded-full h-5 w-5 bg-white border-2 border-sky-500 absolute",
+          "rounded-full h-5 w-5 bg-white border-2 border-sky-600 absolute",
           right && "right-0"
         )}
       ></div>
