37fa471abbf8badc40fc26864b7378f02ba6c4b6
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-07-17 20:37:29 +1000

    Update component

--- src/component/SearchResultGridItem.tsx
+++ src/component/SearchResultGridItem.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { flickrFeedItemInterface } from "./utility/extractFlickrFeedData";
+import { CustomModal } from "./CustomModal";
 
 interface SearchResultItemProps {
   author: string;
@@ -13,6 +15,13 @@ export const SearchResultGridItem = ({
   tags,
   thumbnail,
 }: SearchResultItemProps) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [image, setImage] = useState("");
+
+  const handleClick = () => {
+    setModalIsOpen(!modalIsOpen);
+  };
+
   const getAuthor = () => {
     return author.split('"')[1];
   };
@@ -22,27 +31,43 @@ export const SearchResultGridItem = ({
     return date.toLocaleDateString();
   };
 
+  const getImage = () => {
+    const thumbnailUrl = thumbnail.m as string;
+    const imageUrl = thumbnailUrl.replace("_m", "_b");
+    setImage(imageUrl);
+    handleClick();
+  };
+
   const tagsCollection = tags.split(" ");
 
   return (
-    <a
-      href="/"
-      className="border-2 rounded-xl overflow-hidden hover:shadow-md"
-      data-testid="item"
-    >
-      <div
-        data-testid="thumbnail"
-        className="bg-cover h-36 ease-in duration-200 hover:scale-110"
-        style={{ backgroundImage: `url(${thumbnail.m})` }}
+    <>
+      <button
+        className="border-2 rounded-xl overflow-hidden text-left hover:shadow-md"
+        data-testid="item"
+        onClick={getImage}
       >
-        <span className="inline-block m-4 px-1 py-0.5 text-xs rounded-md bg-pink-300 text-pink-800">
-          {tagsCollection.length} tags
-        </span>
-      </div>
-      <div className="p-4">
-        <div className="text-lg font-bold text-pink-800">{getAuthor()}</div>
-        <div className="text-xs text-gray-500">{getDateTaken()}</div>
-      </div>
-    </a>
+        <div
+          data-testid="thumbnail"
+          className="bg-cover bg-center h-36 ease-in duration-200 hover:scale-110"
+          style={{ backgroundImage: `url(${thumbnail.m})` }}
+        >
+          <span className="inline-block m-4 px-1 py-0.5 text-xs rounded-md bg-pink-300 text-pink-800">
+            {tagsCollection.length} tags
+          </span>
+        </div>
+        <div className="p-4">
+          <div className="text-lg font-bold text-pink-800">{getAuthor()}</div>
+          <div className="text-xs text-gray-500">{getDateTaken()}</div>
+        </div>
+      </button>
+      <CustomModal
+        modalIsOpen={modalIsOpen}
+        imageUrl={image}
+        author={getAuthor()}
+        dateTaken={getDateTaken()}
+        onClick={handleClick}
+      />
+    </>
   );
 };
--- src/component/SearchResultListItem.tsx
+++ src/component/SearchResultListItem.tsx
@@ -1,4 +1,6 @@
 import { flickrFeedItemInterface } from "./utility/extractFlickrFeedData";
+import { CustomModal } from "./CustomModal";
+import { useState } from "react";
 
 interface SearchResultItemProps {
   author: string;
@@ -13,6 +15,13 @@ export const SearchResultListItem = ({
   thumbnail,
   children,
 }: SearchResultItemProps) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [image, setImage] = useState("");
+
+  const handleClick = () => {
+    setModalIsOpen(!modalIsOpen);
+  };
+
   const getAuthor = () => {
     return author.split('"')[1];
   };
@@ -22,29 +31,47 @@ export const SearchResultListItem = ({
     return date.toLocaleDateString();
   };
 
+  const getImage = () => {
+    const thumbnailUrl = thumbnail.m as string;
+    const imageUrl = thumbnailUrl.replace("_m", "_b");
+    setImage(imageUrl);
+    handleClick();
+  };
+
   return (
-    <a
-      href="/"
-      className="flex border-2 rounded-xl overflow-hidden hover:shadow-md"
-      data-testid="item"
-    >
-      <div
-        data-testid="thumbnail"
-        className="basis-80 shrink-0 bg-cover w-64 min-h-[10rem] ease-in duration-200 hover:scale-110"
-        style={{ backgroundImage: `url(${thumbnail.m})` }}
-      ></div>
-      <div className="p-4 w-full">
-        <div className="flex justify-between">
-          <div>
-            <div className="text-lg font-bold text-pink-800">{getAuthor()}</div>
-            <div className="text-xs text-gray-500 mb-8">{getDateTaken()}</div>
+    <>
+      <button
+        className="flex border-2 text-left rounded-xl overflow-hidden hover:shadow-md"
+        data-testid="item"
+        onClick={getImage}
+      >
+        <div
+          data-testid="thumbnail"
+          className="h-full basis-80 shrink-0 bg-cover bg-center w-64 min-h-[10rem] ease-in duration-200 hover:scale-110"
+          style={{ backgroundImage: `url(${thumbnail.m})` }}
+        ></div>
+        <div className="p-4 w-full">
+          <div className="flex justify-between items-start">
+            <div>
+              <div className="text-lg font-bold text-pink-800">
+                {getAuthor()}
+              </div>
+              <div className="text-xs text-gray-500 mb-8">{getDateTaken()}</div>
+            </div>
+            <button className="text-lg font-bold border rounded-full px-4 border-pink-800 text-pink-800 hover:bg-pink-800 hover:text-white">
+              view
+            </button>
           </div>
-          <button className="h-full bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full">
-            Click to View
-          </button>
+          {children}
         </div>
-        {children}
-      </div>
-    </a>
+      </button>
+      <CustomModal
+        modalIsOpen={modalIsOpen}
+        imageUrl={image}
+        author={getAuthor()}
+        dateTaken={getDateTaken()}
+        onClick={handleClick}
+      />
+    </>
   );
 };
